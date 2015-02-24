cb5acfec1b4d6479336dac5814bfbefa0a32479f
jameshedaweng <heda.weng@gmail.com>
2015-02-24 11:44:28 +0100

    Use Role Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
+require 'role_model'
+
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  include RoleModel
+  roles_attribute :roles_mask
+  roles :admin, :premium
 end
